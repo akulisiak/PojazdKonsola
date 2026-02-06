edfb5883f65d35bb620e97181c918e1d4a9fb13e
Student <Student@TE-KAL-D-S14-03>
2026-02-06 12:56:23 +0100

    Implementacja klasy Pojazd

---
+++ PojazdKonsola/Pojazd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PojazdKonsola
+{
+    abstract class Pojazd
+    {
+        protected string marka;
+        protected ushort rokProdukcji;
+
+        public Pojazd(string marka, ushort rokProdukcji)
+        {
+            this.marka = marka;
+            this.rokProdukcji = rokProdukcji;
+        }
+
+        public abstract double ObliczKosztWynajmu(int dni);
+
+        public virtual string Opis()
+        {
+            return $"Marka: {marka}, rok produkcji: {rokProdukcji}";
+        }
+    }
+}
