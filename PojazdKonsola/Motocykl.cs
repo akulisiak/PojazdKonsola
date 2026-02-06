5009f0ca8e26298aac521c133024c26b9a4d02cf
Student <Student@TE-KAL-D-S14-03>
2026-02-06 13:39:27 +0100

    Implementacja klasy Motocykl

---
+++ PojazdKonsola/Motocykl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PojazdKonsola
+{
+    internal class Motocykl : Pojazd
+    {
+        private bool czyZabytkowy;
+
+        public Motocykl(bool czyZabytkowy, string marka, ushort rokProdukcji) : base(marka, rokProdukcji)
+        {
+            this.czyZabytkowy = czyZabytkowy;
+        }
+
+        public override double ObliczKosztWynajmu(int dni)
+        {
+            double koszt = (70 * dni);
+            if (czyZabytkowy)
+            {
+
+                return (koszt * 0.2);
+            }
+            else
+            {
+                return koszt;
+            }
+        }
+
+        public override string Opis()
+        {
+            string czyZabytek = czyZabytkowy ? "jest" : "nie jest";
+            return base.Opis() + $", Motocykl {czyZabytek} zabytkowy";
+        }
+
+    }
+}
